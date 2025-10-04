dc68f6001aac50348def01aa633632fce81e6f45
om.thakur <om.thakur@juspay.in>
2025-10-05 01:45:20 +0530

    Write changes to src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,6 @@
 //! A FIDO2/WebAuthn conformant server implementation in Rust.
 
 pub mod config;
-pub mod controllers;
-pub mod db;
 pub mod error;
-pub mod middleware;
-pub mod routes;
-pub mod schema;
-pub mod services;
-pub mod utils;
 
 pub use error::{AppError, Result};
