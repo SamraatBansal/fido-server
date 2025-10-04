2a7b1dde61921c4daee9ed452b42c34ef44418f9
om.thakur <om.thakur@juspay.in>
2025-10-04 21:50:40 +0530

    Write changes to src/services/mod.rs

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,1 +1,11 @@
-//! Services module
+//! Business logic services
+
+pub mod credential;
+pub mod fido;
+pub mod session;
+pub mod user;
+
+pub use credential::*;
+pub use fido::*;
+pub use session::*;
+pub use user::*;
