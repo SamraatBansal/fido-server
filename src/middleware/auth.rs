4ac0b395095843af67531349e61d1dca21566266
om.thakur <om.thakur@juspay.in>
2025-10-05 02:05:09 +0530

    Write new file src/middleware/auth.rs

---
+++ src/middleware/auth.rs
@@ -0,0 +1,45 @@
+//! Authentication middleware
+
+use actix_web::{dev::ServiceRequest, Error, Result};
+use actix_web::dev::{Service, ServiceResponse, Transform};
+use std::future::{ready, Ready};
+
+/// Authentication middleware placeholder
+pub struct AuthMiddleware;
+
+impl<S, B> Transform<S, ServiceRequest> for AuthMiddleware
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = AuthMiddlewareService<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(AuthMiddlewareService { service }))
+    }
+}
+
+pub struct AuthMiddlewareService<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for AuthMiddlewareService<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = S::Future;
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        // TODO: Implement authentication logic
+        self.service.call(req)
+    }
+}
