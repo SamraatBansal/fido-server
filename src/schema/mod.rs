357a4c6ce9a43e0874bca9210b4e707094e99106
om.thakur <om.thakur@juspay.in>
2025-10-05 01:30:14 +0530

    Write changes to src/schema/mod.rs

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -1,1 +1,13 @@
-//! Request/Response schema module
+//! Request and response schemas
+
+pub mod auth;
+pub mod registration;
+pub mod credentials;
+pub mod common;
+pub mod admin;
+
+pub use auth::*;
+pub use registration::*;
+pub use credentials::*;
+pub use common::*;
+pub use admin::*;
