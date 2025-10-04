b76ab7f97815799c9cf3f929da264a31c59527c4
om.thakur <om.thakur@juspay.in>
2025-10-04 21:49:08 +0530

    Write changes to src/controllers/mod.rs

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,1 +1,11 @@
-//! Controllers module
+//! HTTP request controllers
+
+pub mod authentication;
+pub mod credentials;
+pub mod health;
+pub mod registration;
+
+pub use authentication::*;
+pub use credentials::*;
+pub use health::*;
+pub use registration::*;
