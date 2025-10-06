8b1a17bdeff6026c8be21c1e80fbb4272bc9c100
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-07 02:15:57 +0530

    Write new file tests/unit/services.rs

---
+++ tests/unit/services.rs
@@ -0,0 +1,24 @@
+//! Service layer unit tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_webauthn_service_creation() {
+        // TODO: Test WebAuthn service creation
+        assert!(true);
+    }
+
+    #[test]
+    fn test_challenge_generation() {
+        // TODO: Test challenge generation
+        assert!(true);
+    }
+
+    #[test]
+    fn test_credential_storage() {
+        // TODO: Test credential storage
+        assert!(true);
+    }
+}
