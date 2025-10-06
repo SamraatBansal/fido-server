12c31e36910363422ac857a0b21f0f1e25a1d40e
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-06 14:36:58 +0530

    Edit src/middleware/mod.rs

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,11 +1,13 @@
 //! HTTP middleware
 
 pub mod auth;
+pub mod client_ip;
 pub mod cors;
 pub mod rate_limit;
 pub mod security;
 
 pub use auth::*;
+pub use client_ip::*;
 pub use cors::*;
 pub use rate_limit::*;
 pub use security::*;
