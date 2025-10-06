b5f246a0f904957ba5e05e699d5ca8db40c8489c
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-06 14:26:08 +0530

    Edit src/services/mod.rs

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,10 +1,12 @@
 //! Business logic services
 
+pub mod attestation;
 pub mod credential;
 pub mod fido;
 pub mod session;
 pub mod user;
 
+pub use attestation::*;
 pub use credential::*;
 pub use fido::*;
 pub use session::*;
