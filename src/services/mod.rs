431f197a134906a526b2f00e1217d0f98846ec28
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-06 18:02:02 +0530

    Write changes to src/services/mod.rs

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,13 +1,5 @@
 //! Business logic services
 
-pub mod attestation;
-pub mod credential;
 pub mod fido;
-pub mod session;
-pub mod user;
 
-pub use attestation::*;
-pub use credential::*;
 pub use fido::*;
-pub use session::*;
-pub use user::*;
