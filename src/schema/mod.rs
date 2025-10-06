d2b69a2ffd749c92e271e2df554ba9155734d6da
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-07 02:18:43 +0530

    Edit src/schema/mod.rs

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -1,1 +1,12 @@
 //! Request/Response schema module
+
+pub mod challenge;
+pub mod credential;
+pub mod user;
+pub mod registration;
+pub mod authentication;
+pub mod common;
+
+pub use registration::*;
+pub use authentication::*;
+pub use common::*;
