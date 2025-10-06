6d12abb41f29496ad0422be4872e29d6bfabdcfb
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-07 02:22:51 +0530

    Edit src/services/mod.rs

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,1 +1,11 @@
 //! Services module
+
+pub mod challenge;
+pub mod credential;
+pub mod user;
+pub mod webauthn;
+
+pub use challenge::*;
+pub use credential::*;
+pub use user::*;
+pub use webauthn::*;
