8a75a9614f91121a2e1d02db7411e3b3286b3af1
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-09 13:50:12 +0530

    EUL-0000 Auto-commit changes from tool execution chatcmpl-tool-cdabf75acd9d43359f01cd02732eaec2

--- src/services/credential.rs
+++ src/services/credential.rs
@@ -16,25 +16,25 @@ impl CredentialService {
     }
 
     /// Create a new credential
-    pub async fn create_credential(&self, credential: &Credential) -> FidoResult<Credential> {
+    pub async fn create_credential(&self, _credential: &Credential) -> FidoResult<Credential> {
         // TODO: Implement credential creation
         Err(FidoError::Internal("Not implemented".to_string()))
     }
 
     /// Find credential by credential ID
-    pub async fn find_by_credential_id(&self, credential_id: &str) -> FidoResult<Option<Credential>> {
+    pub async fn find_by_credential_id(&self, _credential_id: &str) -> FidoResult<Option<Credential>> {
         // TODO: Implement credential lookup
         Ok(None)
     }
 
     /// Find credentials by user ID
-    pub async fn find_by_user_id(&self, user_id: &Uuid) -> FidoResult<Vec<Credential>> {
+    pub async fn find_by_user_id(&self, _user_id: &Uuid) -> FidoResult<Vec<Credential>> {
         // TODO: Implement credential lookup by user
         Ok(vec![])
     }
 
     /// Update credential sign count
-    pub async fn update_sign_count(&self, credential_id: &Uuid, sign_count: u64) -> FidoResult<()> {
+    pub async fn update_sign_count(&self, _credential_id: &Uuid, _sign_count: u64) -> FidoResult<()> {
         // TODO: Implement sign count update
         Ok(())
     }
