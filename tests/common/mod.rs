19102502a03e00367284abed20485310b4894237
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-15 04:15:46 +0530

    EUL-0000 Auto-commit changes from tool execution toolu_vrtx_01GMbiWxhYxny9Z1cwbY35U4

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -212,6 +212,7 @@ pub fn validate_assertion_options_response(response: &ServerPublicKeyCredentialG
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::json;
 
     #[test]
     fn test_generate_test_challenge() {
