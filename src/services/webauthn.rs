7e2607d034eb6ceba5b85e7a6b03e91cd389ec35
harimohan.sharma <harimohan.sharma@juspay.in>
2025-10-17 20:58:18 +0530

    Auto-commit changes from tool execution chatcmpl-tool-5ca2ebe72eb34347bd6c74b62fd98064

--- src/services/webauthn.rs
+++ src/services/webauthn.rs
@@ -315,7 +315,9 @@ impl WebAuthnService {
                 let rp_id_hash = &authenticator_data_bytes[0..32];
                 // For localhost tests, this should be a hash of "localhost"
                 let expected_rp_id = "localhost";
-                let expected_hash = sha2::Sha256::digest(expected_rp_id.as_bytes());
+                let mut hasher = sha2::Sha256::new();
+                hasher.update(expected_rp_id.as_bytes());
+                let expected_hash = hasher.finalize();
                 if rp_id_hash != expected_hash.as_slice() {
                     // For conformance tests, we need to be more flexible
                     // But still validate it's a proper hash (not all zeros)
