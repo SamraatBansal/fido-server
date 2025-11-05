8caca6df30c8060fc378500dc62bf66b14e0310e
om.thakur <om.thakur@juspay.in>
2025-11-05 19:51:21 +0530

    Auto-commit changes from tool execution toolu_vrtx_015fv7U2ypobafXAV64kkAG1

--- src/handlers/attestation.rs
+++ src/handlers/attestation.rs
@@ -163,7 +163,7 @@ pub async fn result(
     let reg_credential = RegisterPublicKeyCredential {
         id: credential.id.clone(),
         raw_id: credential_id.into(),
-        response: AuthenticatorAttestationResponseRaw {
+        response: webauthn_rs::prelude::AuthenticatorAttestationResponseRaw {
             client_data_json,
             attestation_object,
         },
