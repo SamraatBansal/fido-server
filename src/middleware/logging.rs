176d4392d58bf3758897e9718a26c2bfbec0ccf7
om.thakur <om.thakur@juspay.in>
2025-11-05 19:23:39 +0530

    Auto-commit changes from tool execution toolu_vrtx_01Dd6u3xpPBhgbpK5dyyxdcm

--- src/middleware/logging.rs
+++ src/middleware/logging.rs
@@ -1,12 +1,7 @@
 use tower_http::trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tracing::{Level, Span};
 
-pub fn logging_layer() -> TraceLayer<
-    tower_http::classify::SharedClassifier<tower_http::classify::ServerErrorsAsFailures>,
-    DefaultOnRequest<Level>,
-    DefaultOnResponse<Level>,
-    tower_http::trace::DefaultOnFailure<Level>,
-> {
+pub fn logging_layer() -> TraceLayer<tower_http::classify::SharedClassifier<tower_http::classify::ServerErrorsAsFailures>> {
     TraceLayer::new_for_http()
         .on_request(DefaultOnRequest::new().level(Level::INFO))
         .on_response(DefaultOnResponse::new().level(Level::INFO))
