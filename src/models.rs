c1c5420c5f640955575efea876024c9c605c9c36
om.thakur <om.thakur@juspay.in>
2025-11-05 19:49:47 +0530

    Auto-commit changes from tool execution toolu_vrtx_017fgcZkoSMq7vUA9irM2fEE

--- src/models.rs
+++ src/models.rs
@@ -26,7 +26,7 @@ pub struct AttestationOptionsResponse {
     pub user: ServerPublicKeyCredentialUserEntity,
     pub challenge: String,
     #[serde(rename = "pubKeyCredParams")]
-    pub pub_key_cred_params: Vec<webauthn_rs::prelude::PublicKeyCredentialParameters>,
+    pub pub_key_cred_params: Vec<serde_json::Value>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub timeout: Option<u32>,
     #[serde(rename = "excludeCredentials", default)]
