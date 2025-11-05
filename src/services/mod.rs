0453562970c5cbb4081a7b2e2f544aa5186ffb00
om.thakur <om.thakur@juspay.in>
2025-11-05 19:14:42 +0530

    Auto-commit changes from tool execution toolu_vrtx_01ULXaLoUPaxgXW9eue6gNLR

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,1 +1,9 @@
-//! Services module
+pub mod webauthn;
+pub mod challenge;
+pub mod user;
+pub mod credential;
+
+pub use webauthn::WebAuthnService;
+pub use challenge::ChallengeService;
+pub use user::UserService;
+pub use credential::CredentialService;
