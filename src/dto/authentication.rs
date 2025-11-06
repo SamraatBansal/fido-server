1ec89ba929c3a993d45c7e1c885ecdf9cf3f4959
om.thakur <om.thakur@juspay.in>
2025-11-06 12:36:04 +0530

    Auto-commit changes from tool execution toolu_vrtx_01RsZKm7bL6U2dkDeSydUgAJ

---
+++ src/dto/authentication.rs
@@ -0,0 +1,41 @@
+//! Authentication DTO types
+
+use serde::{Deserialize, Serialize};
+use webauthn_rs::prelude::*;
+use super::common::*;
+
+/// Server public key credential get options request
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerPublicKeyCredentialGetOptionsRequest {
+    pub username: String,
+    #[serde(rename = "userVerification", skip_serializing_if = "Option::is_none")]
+    pub user_verification: Option<UserVerificationPolicy>,
+}
+
+/// Server public key credential get options response
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerPublicKeyCredentialGetOptionsResponse {
+    #[serde(flatten)]
+    pub server_response: ServerResponse,
+    pub challenge: String, // base64url encoded
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub timeout: Option<u32>,
+    #[serde(rename = "rpId", skip_serializing_if = "Option::is_none")]
+    pub rp_id: Option<String>,
+    #[serde(rename = "allowCredentials", default)]
+    pub allow_credentials: Vec<ServerPublicKeyCredentialDescriptor>,
+    #[serde(rename = "userVerification", skip_serializing_if = "Option::is_none")]
+    pub user_verification: Option<UserVerificationPolicy>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub extensions: Option<serde_json::Value>,
+}
+
+/// Authentication result request (from client)
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthenticationResultRequest {
+    #[serde(flatten)]
+    pub credential: ServerPublicKeyCredential,
+}
+
+/// Authentication result response (to client)
+pub type AuthenticationResultResponse = ServerResponse;
