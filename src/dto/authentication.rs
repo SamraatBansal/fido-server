a046a3db8b8c671efe47e51e27aed2f2ecd6192b
om.thakur <om.thakur@juspay.in>
2025-11-06 13:18:13 +0530

    Auto-commit changes from tool execution toolu_vrtx_01MLMEWZFAerzA3tURYMEfPg

--- src/dto/authentication.rs
+++ src/dto/authentication.rs
@@ -1,15 +1,17 @@
-//! Authentication DTO types
+//! Authentication DTO types for FIDO2/WebAuthn compliance
 
 use serde::{Deserialize, Serialize};
 use webauthn_rs_proto::UserVerificationPolicy;
 use super::common::*;
 
-/// Server public key credential get options request
+/// Server public key credential get options request  
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ServerPublicKeyCredentialGetOptionsRequest {
     pub username: String,
     #[serde(rename = "userVerification", skip_serializing_if = "Option::is_none")]
     pub user_verification: Option<UserVerificationPolicy>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub extensions: Option<serde_json::Value>,
 }
 
 /// Server public key credential get options response
@@ -22,7 +24,7 @@ pub struct ServerPublicKeyCredentialGetOptionsResponse {
     pub timeout: Option<u32>,
     #[serde(rename = "rpId", skip_serializing_if = "Option::is_none")]
     pub rp_id: Option<String>,
-    #[serde(rename = "allowCredentials", default)]
+    #[serde(rename = "allowCredentials")]
     pub allow_credentials: Vec<ServerPublicKeyCredentialDescriptor>,
     #[serde(rename = "userVerification", skip_serializing_if = "Option::is_none")]
     pub user_verification: Option<UserVerificationPolicy>,
