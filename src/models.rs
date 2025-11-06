6fe658f38eec6187d5bab8cd9e3135dae21c681b
om.thakur <om.thakur@juspay.in>
2025-11-06 19:16:55 +0530

    Auto-commit changes from tool execution toolu_vrtx_01G8Gk3hAiryKZAAwzbFRKXX

--- src/models.rs
+++ src/models.rs
@@ -3,8 +3,10 @@ use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Queryable, Identifiable, Serialize, Deserialize, Clone)]
-#[diesel(table_name = crate::schema::users)]
+use crate::schema::*;
+
+#[derive(Debug, Clone, Queryable, Identifiable, Serialize, Deserialize)]
+#[diesel(table_name = users)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
@@ -13,54 +15,81 @@ pub struct User {
     pub updated_at: DateTime<Utc>,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = crate::schema::users)]
+#[derive(Debug, Clone, Insertable)]
+#[diesel(table_name = users)]
 pub struct NewUser {
+    pub id: Uuid,
     pub username: String,
     pub display_name: String,
 }
 
-#[derive(Queryable, Identifiable, Associations, Serialize, Deserialize, Clone)]
+#[derive(Debug, Clone, Queryable, Identifiable, Associations, Serialize, Deserialize)]
 #[diesel(belongs_to(User))]
-#[diesel(table_name = crate::schema::credentials)]
+#[diesel(table_name = credentials)]
 pub struct Credential {
     pub id: Uuid,
     pub user_id: Uuid,
     pub credential_id: Vec<u8>,
     pub public_key: Vec<u8>,
     pub sign_count: i64,
-    pub transports: Option<String>,
+    pub transports: Option<Vec<String>>,
+    pub backup_eligible: bool,
+    pub backup_state: bool,
     pub created_at: DateTime<Utc>,
     pub last_used: Option<DateTime<Utc>>,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = crate::schema::credentials)]
+#[derive(Debug, Clone, Insertable)]
+#[diesel(table_name = credentials)]
 pub struct NewCredential {
+    pub id: Uuid,
     pub user_id: Uuid,
     pub credential_id: Vec<u8>,
     pub public_key: Vec<u8>,
     pub sign_count: i64,
-    pub transports: Option<String>,
+    pub transports: Option<Vec<String>>,
+    pub backup_eligible: bool,
+    pub backup_state: bool,
 }
 
-#[derive(Queryable, Identifiable, Associations)]
+#[derive(Debug, Clone, Queryable, Identifiable, Associations)]
 #[diesel(belongs_to(User))]
-#[diesel(table_name = crate::schema::challenges)]
-pub struct Challenge {
+#[diesel(table_name = registration_challenges)]
+pub struct RegistrationChallenge {
     pub id: Uuid,
-    pub user_id: Option<Uuid>,
-    pub challenge_type: String,
-    pub challenge_data: Vec<u8>,
+    pub user_id: Uuid,
+    pub challenge: Vec<u8>,
+    pub state_data: Vec<u8>,
     pub expires_at: DateTime<Utc>,
     pub created_at: DateTime<Utc>,
 }
 
-#[derive(Insertable)]
-#[diesel(table_name = crate::schema::challenges)]
-pub struct NewChallenge {
-    pub user_id: Option<Uuid>,
-    pub challenge_type: String,
-    pub challenge_data: Vec<u8>,
+#[derive(Debug, Clone, Insertable)]
+#[diesel(table_name = registration_challenges)]
+pub struct NewRegistrationChallenge {
+    pub user_id: Uuid,
+    pub challenge: Vec<u8>,
+    pub state_data: Vec<u8>,
+    pub expires_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Queryable, Identifiable, Associations)]
+#[diesel(belongs_to(User))]
+#[diesel(table_name = authentication_challenges)]
+pub struct AuthenticationChallenge {
+    pub id: Uuid,
+    pub user_id: Uuid,
+    pub challenge: Vec<u8>,
+    pub state_data: Vec<u8>,
+    pub expires_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Insertable)]
+#[diesel(table_name = authentication_challenges)]
+pub struct NewAuthenticationChallenge {
+    pub user_id: Uuid,
+    pub challenge: Vec<u8>,
+    pub state_data: Vec<u8>,
     pub expires_at: DateTime<Utc>,
 }
