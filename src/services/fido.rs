6039203cc65fa03abb6d566479b32163ba7f5823
om.thakur <om.thakur@juspay.in>
2025-11-06 12:44:48 +0530

    Auto-commit changes from tool execution toolu_vrtx_0168Bu5BY14t6t8LgwCFz5aE

--- src/services/fido.rs
+++ src/services/fido.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use url::Url;
 use webauthn_rs::prelude::*;
-// We'll define our own types for now
+use crate::dto::registration::{PublicKeyCredentialParameters, PublicKeyCredentialRpEntity};
 
 use crate::config::settings::WebAuthnSettings;
 use crate::db::{models::*, DbPool};
