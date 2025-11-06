8ab4a6c61ce54d4f570cab1a7e62756ff93ab65f
om.thakur <om.thakur@juspay.in>
2025-11-06 15:40:43 +0530

    Auto-commit changes from tool execution toolu_vrtx_01VjPy2UDxWnNi8N9us8A3cf

--- src/conformance_handlers.rs
+++ src/conformance_handlers.rs
@@ -1,6 +1,7 @@
 use crate::api::*;
 use crate::error::Result;
 use crate::conformance_service::ConformanceWebAuthnService;
+use crate::json_extractor_fixed::JsonExtractor;
 use actix_web::{web, HttpResponse};
 
 pub async fn start_registration(
