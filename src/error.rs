3cf39f73f120830f173dbb4643417d3eef0d6971
om.thakur <om.thakur@juspay.in>
2025-11-06 13:21:05 +0530

    Auto-commit changes from tool execution toolu_vrtx_01X2uGEnm4HqAFCA4cHX8U6r

--- src/error.rs
+++ src/error.rs
@@ -48,8 +48,8 @@ impl fmt::Display for WebAuthnError {
 
 impl std::error::Error for WebAuthnError {}
 
-impl From<webauthn_rs::error::WebauthnError> for WebAuthnError {
-    fn from(err: webauthn_rs::error::WebauthnError) -> Self {
+impl From<webauthn_rs::WebauthnError> for WebAuthnError {
+    fn from(err: webauthn_rs::WebauthnError) -> Self {
         WebAuthnError::WebAuthn(err)
     }
 }
