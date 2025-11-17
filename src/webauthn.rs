29cbed424e7f4d10e7b6c6b1a08323804574a964
om.thakur <om.thakur@juspay.in>
2025-11-17 18:58:23 +0530

    Auto-commit changes from tool execution toolu_vrtx_01GgsZkC4xG62swTdiNbvHS4

--- src/webauthn.rs
+++ src/webauthn.rs
@@ -15,7 +15,7 @@ pub struct WebAuthnService {
 }
 
 impl WebAuthnService {
-    pub fn new(rp_id: &str, origin: &url::Url, rp_name: &str, db: Database) -> Result<Self> {
+    pub fn new(rp_id: &str, origin: &url::Url, rp_name: &str, db: MemoryDatabase) -> Result<Self> {
         let webauthn = WebauthnBuilder::new(rp_id, origin)?
             .rp_name(rp_name)
             .build()?;
