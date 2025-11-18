0a9f3ab1c471eaa192598f14be2a91e63c300f35
om.thakur <om.thakur@juspay.in>
2025-11-18 16:20:54 +0530

    Auto-commit changes from tool execution toolu_vrtx_01YF3cG4uSt2uYQDf6w5dsy7

--- src/services/fido.rs
+++ src/services/fido.rs
@@ -236,7 +236,7 @@ impl FidoService {
         });
 
         let challenge_state = NewChallengeState {
-            challenge: challenge.as_ref().to_vec(),
+            challenge: challenge_bytes.to_vec(),
             user_id: Some(user.id),
             operation: ChallengeOperation::Authentication.to_string(),
             state_data,
